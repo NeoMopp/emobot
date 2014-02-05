74f905f471f545618c5520ddc62f07ebd382e4d0
NeoMopp <willzsawyer@gmail.com>
2014-02-05 21:01:18 +0000

    Changes made to python file to allow for compiling

--- ledOnOff.py
+++ ledOnOff.py
@@ -3,14 +3,14 @@ import serial;
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
-	t = input('Light on or off, 1 for on 0 for off')
+	t = input('Light on or off, 1 for on 0 for off:		')
 
 	if t == 1:
-		//turn led on
+		#turn led on
 		ser.write('c1')
 
-	else if t == 0:
-		//turn led off
+	elif t == 0:
+		#turn led off
 		ser.write('c0')
 
 	else:
