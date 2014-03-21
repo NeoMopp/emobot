7c210bcef2c9df802fa5489f9f8fe48f7ba74c2d
NeoMopp <willzsawyer@gmail.com>
2014-03-21 11:00:05 +0000

    Changes made to allow for communication over serial

--- core_prog/main.cpp
+++ core_prog/main.cpp
@@ -18,16 +18,66 @@
 #include <stdio.h>
 
 
+//additional includes so we can send commands to serial
+#include <termios.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+
 using namespace std;
 using namespace cv;
+
+
+//variables that will be used across the entire program
+int fd;		 	//needed for the serial write
+double happiness;	//Keepon's happiness rating
+
+public void sendAction()
+{
+	char msg[] = "SOUND PLAY 63;";	
+	write(fd, msg, strlen(msg));
+}
+
+
 
 
 //Main of program argc is the number of arguments while argv is the arguments
 //said argument are normally locations of files
 int main(int argc, char* argv[])
 {
+	//initalize variables
+	happiness = 0;
+	fd = open("/dev/ttyACM0", O_RDWR);
+	if (fd == -1) 
+	{
+  		perror("/dev/ttyACM0");
+  		cout<<"Failed to connect";
+  		return 1;
+	}
+
+	//Set up the serial port to allow for writing to it.
+	struct termios tios;
+	tcgetattr(fd, &tios);
+	// disable flow control and all that, and ignore break and parity errors
+	tios.c_iflag = IGNBRK | IGNPAR;
+	tios.c_oflag = 0;
+	tios.c_lflag = 0;
+	cfsetspeed(&tios, B115200);
+	tcsetattr(fd, TCSAFLUSH, &tios);
 	
+	// the serial port has a brief glitch once we turn it on which generates a
+	// start bit; sleep for 1ms to let it settle
+	sleep(1000);
 	
+	int i;
+	cin>>i;
+	//Need to tell user to connect keepon up and check we can connect
+	sendAction();
+
+
 	//create classifier and load a cascade file into it, to allow for face detection.
 	CascadeClassifier face_cascade;
 	face_cascade.load("haarcascade_frontalface_alt.xml");
@@ -67,17 +117,14 @@ int main(int argc, char* argv[])
 		
 		face_cascade.detectMultiScale(greyscaleFrame, faces, 1.1, 3, CV_HAAR_SCALE_IMAGE, Size(30,30));
 		imshow("outputCapture", captureFrame);
-		//Write to text file
-		if (faces.size() > 0)
+				
+		//check for faces and change value of happiness dependent of if there is a face.
+		if (faces.size() > 1)
 		{
-			ofstream myfile ("faces.txt");
-			if (myfile.is_open())
-			{
-				myfile<< "1";
-				myfile.close();
-			}
-			else cout<< "Unable to openfile";
+			happiness += 10.0;
 		}
+		else 
+			happiness -= 0.5;
 
 		if (waitKey(10) == 27)
 		{
